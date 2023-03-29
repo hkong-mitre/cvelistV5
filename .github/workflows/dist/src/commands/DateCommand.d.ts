4d43cb95648d2d9e6432320aeec2895ca0cbc68b
Howard Kong <hkong@mitre.org>
2023-03-28 20:49:48 -0400

    new cve utils

--- .github/workflows/dist/src/commands/DateCommand.d.ts
+++ .github/workflows/dist/src/commands/DateCommand.d.ts
@@ -3,5 +3,4 @@ import { GenericCommand } from './GenericCommand.js';
 export declare class DateCommand extends GenericCommand {
     constructor(name: string, program: Command);
     run(options: any): Promise<void>;
-    static getIsoDate(timestamp?: Date): string;
 }
