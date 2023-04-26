f92b8bd64686546b9ad3cfd0bd10a850e420de07
hkong-mitre <122547078+hkong-mitre@users.noreply.github.com>
2023-04-26 10:48:52 -0400

    new dist with twitter command

--- .github/workflows/dist/src/commands/TwitterCommand.d.ts
+++ .github/workflows/dist/src/commands/TwitterCommand.d.ts
@@ -1,7 +1,7 @@
 import { Command } from 'commander';
 import { GenericCommand } from './GenericCommand.js';
 /** Command to print out current date in various formats */
-export declare class DateCommand extends GenericCommand {
+export declare class TwitterCommand extends GenericCommand {
     constructor(name: string, program: Command);
     run(options: any): Promise<void>;
 }
